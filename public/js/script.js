bc2cbcfa29975b6ec2b1fcb0e5f1919a991d413f
Gaab <154674155+GaabDevPy@users.noreply.github.com>
2025-05-14 20:59:59 -0300

    Create script.js

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+const socket = io();
+const statusMessage = document.getElementById('status-message');
+const qrcodeImg = document.getElementById('qrcode');
+const connectBtn = document.getElementById('connect-btn');
+const qrcodeContainer = document.getElementById('qrcode-container');
