e93b3be1371a18c8f891393c3b65be344f44211e
Gaab <154674155+GaabDevPy@users.noreply.github.com>
2025-05-14 20:58:39 -0300

    Update dashboard.js

---
+++ public/js/dashboard.js
@@ -0,0 +1,3 @@
+import { initMessagesSection, loadMessages } from './modules/messages.js';
+import { initContactsSection, loadContacts } from './modules/contacts.js';
+import { initMassSendSection } from './modules/massSend.js';
