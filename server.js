388c4124defbcc469ece3dce259678bbafc8f281
Gaab <154674155+GaabDevPy@users.noreply.github.com>
2025-05-14 20:50:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -21,3 +21,9 @@ app.use(express.static(path.join(__dirname, 'public'), {
     }
   }
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
