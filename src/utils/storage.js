edb053c3a76fd5f0a5effb026f02b9ffe631cc72
Gaab <154674155+GaabDevWeb@users.noreply.github.com>
2025-05-22 13:42:39 -0300

    Update storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -7,3 +7,12 @@ const __dirname = path.dirname(__filename);
 
 const messagesPath = path.join(__dirname, '../../db', 'messages.json');
 const contactsPath = path.join(__dirname, '../../db', 'contacts.json');
+
+export function readJSON(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    return [];
+  }
+}
