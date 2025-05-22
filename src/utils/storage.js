916fbc776312ec6a8b1d25bb5def22b58f59b87b
Gaab <154674155+GaabDevWeb@users.noreply.github.com>
2025-05-22 13:42:28 -0300

    Update storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -4,3 +4,6 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const messagesPath = path.join(__dirname, '../../db', 'messages.json');
+const contactsPath = path.join(__dirname, '../../db', 'contacts.json');
